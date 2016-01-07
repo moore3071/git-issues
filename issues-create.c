d942fe5f817e82fdf3572287f15fa798e5d24bba
bam <moore.3071@osu.edu>
2016-01-06 23:33:10 -0500

    Starting to make the building blocks and visualize it
    Getting a better understanding of POSIX file creation. No longer
    using stat and instead relying on mkdir's and open's errors to
    determine if files already exist. This should avoid race conditions
    that would cause major problems later for refactoring. Still need
    to scour the man pages for better ways to recognize pathnames for
    Unix systems.
    
    I've decided to use system calls in this despite the slight loss
    to portability (sorry amiga and Plan9, less sorry Windows), but
    like Git, if people like this they can port it over.

--- issues-create.c
+++ issues-create.c
@@ -5,18 +5,24 @@
 struct stat st = {0};
 
 /* Creates an issues file named name in directory dir. The options in query_options will be used to decide what issues to get. If the options * are NULL, we'll account for that later. Sleep for now
- */
+*/
 int
 issues-create(const char * dir, const char * name, struct query_options options)
 {
-	char * dirpath;
 	if(dir!=NULL)
 	{
-	if(stat("", &st) == -1) {
-		mkdir("", 0700);
-	} else {
-		fprintf(stderr, "Directory already exists");
+		if(mkdir(dir,0777)==-1)
+			if(errno != EEXIST)
+			{
+				perror("issues-create");
+				exit(EXIT_FAILURE);
+			}
 	}
-	} else {
-	}
+	get_issues_path(*dir, *name);
+}
+
+void get_issues_path(char **dir. char** file)
+{
+	if(*dir==NULL)
+		return;
 }
